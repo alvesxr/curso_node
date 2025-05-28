fc982e3219ebd8dff5c17e46c5f4252374eb315e
João Rafael Alves Recco <reccoalves10@gmail.com>
2025-05-28 20:29:00 -0300

    CRUD de usuários feito

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -22,6 +22,70 @@ class UserController {
       return res.status(500).json({ error: "Erro interno do servidor" });
     }
   }
+
+  //index
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      console.error(e);
+      return res
+        .status(500)
+        .json({ error: "Erro interno do servidor", details: e.message });
+    }
+  }
+
+  //show
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (e) {
+      console.error(e);
+      return res
+        .status(500)
+        .json({ error: "Erro interno do servidor", details: e.message });
+    }
+  }
+
+  //update
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({ error: ["ID não enviado"] });
+      }
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(404).json({ error: ["Usuário não encontrado"] });
+      }
+
+      const novosDados = await user.update(req.body);
+      return res.json(novosDados);
+    } catch (e) {
+      console.error(e);
+      return res.json(null);
+    }
+  }
+
+  //delete
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({ error: ["ID não enviado"] });
+      }
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(404).json({ error: ["Usuário não encontrado"] });
+      }
+
+      await user.destroy();
+      return res.json(user);
+    } catch (e) {
+      console.error(e);
+      return res.json(null);
+    }
+  }
 }
 
 export default new UserController();
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,10 @@ import user from "../controllers/user.js";
 const router = new Router();
 
 router.post("/", user.store);
+router.get("/", user.index);
+router.get("/:id", user.show);
+router.put("/:id", user.update);
+router.delete("/:id", user.delete);
 
 export default router;
 
